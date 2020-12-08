a69a70bd66b3b127e43e9da0cf392c4d405e669b
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-08 17:27:22 +0000

    Get and evaluate the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = 32426 #random.randint(-10000, 10000)
+lastdigit = abs(number) % 10 #Use abs to get the absolute value
+# print(number, lastdigit)
+if number < 0:
+        lastdigit *= -1
+
+if lastdigit > 5:
+        show = 'greater than 5'
+elif lastdigit == 0:
+        show = 0
+elif lastdigit < 6:
+        show = 'and is less than 6 and not 0'
+
+print("Last digit of {:d} is {:d} and is {:s}".format(number, lastdigit, show))
