6be731e65a04ea04540f124f7ad48011ecb259f2
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-10 18:48:48 +0000

    Fix: Printing format

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
         b = int(argv[3])
 
         if argv[2] == '+' or argv[2] == '-':
-                print("{} + {} = {}".format(a, b, add(a, b)))
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif argv[2] == '-':
-                print("{} - {} = {}".format(a, b, sub(a, b)))
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif argv[2] == '*' or argv[2] == '/':
-                print("{} * {} = {}".format(a, b, mul(a, b)))
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif argv[2] == '/':
-                print("{} / {} = {}".format(a, b, div(a, b)))
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
                 print("Unknown operator. Available operators: +, -, * and /")
                 exit(1)
