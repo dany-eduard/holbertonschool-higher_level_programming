737dd251bcfdd6e2f349b648cbed609aba8e6c95
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-10 17:24:13 +0000

    Fix: If it's just an argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
         length = len(sys.argv)
         if length == 1:
                 print("0 arguments.")
-        elif length > 1:
+        elif length == 2 :
+                print("1 argument.")
+                print("{}: {}".format(1, sys.argv[1]))
+        elif length > 2:
                 print("{} arguments:".format(length - 1))
                 for i in range(1, length):
                         print("{}: {}".format(i, sys.argv[i]))
