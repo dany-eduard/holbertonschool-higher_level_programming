d923847e4d1aa1e5cbaa99e578dea48fdaa8b1b7
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-11 14:12:55 +0000

    Fix: operators sub and div

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,10 +10,14 @@ if __name__ == "__main__":
         a = int(argv[1])
         b = int(argv[3])
 
-        if argv[2] == '+' or argv[2] == '-':
-                print("{:d} {} {:d} = {:d}".format(a, b, argv[2], add(a, b)))
-        elif argv[2] == '*' or argv[2] == '/':
-                print("{:d} {} {:d} = {:d}".format(a, b, argv[2] mul(a, b)))
+        if argv[2] == '+':
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif argv[2] == '-':
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif argv[2] == '/':
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
                 print("Unknown operator. Available operators: +, -, * and /")
                 exit(1)
