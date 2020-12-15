d907969838d03d707850787063953058b08f9dd1
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-15 18:28:51 +0000

    Fix:

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@
 def no_c(my_string):
     new_string = ''
     for i in my_string:
-        if not i in ['c', 'C']:
+        if not (i in ['c', 'C']):
             new_string += i
     return new_string
