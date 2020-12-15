a8954e2c8a69c4f84f0804469625a5ec04301265
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-15 19:40:46 +0000

    Fix: If the sentence is empty the first character should be equal to None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
+    if length <= 0:
+        first = None
+    else:
+        first = sentence[0]
     return length, first
