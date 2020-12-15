9690a9f52f14224add7858a89e3dead75166b570
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-15 18:39:28 +0000

    Remove: if not ... PEP8 does not allow that

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,7 @@
 def no_c(my_string):
     new_string = ''
     for i in my_string:
-        if not i in ['c', 'C']:
-            new_string += i
+        if i in ['c', 'C']:
+            continue
+        new_string += i
     return new_string
