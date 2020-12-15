1d9f6a1dae6baa5362a543237f42002caea1d3f0
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-15 17:33:03 +0000

    Fix: PEP8 Style W191 indentation contains tabs

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,10 @@
 
 
 def new_in_list(my_list, idx, element):
-	new_list = my_list.copy()
-	if idx < 0 or idx > len(my_list):
-		return new_list
-	else:
-		new_list.remove(idx)
-		new_list.insert(idx, element)
-		return new_list
+    new_list = my_list.copy()
+    if idx < 0 or idx > len(my_list):
+        return new_list
+    else:
+        new_list.remove(idx)
+        new_list.insert(idx, element)
+        return new_list
