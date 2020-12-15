0b0ccd4d55e46bb954075961a07f5de26393e73b
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2020-12-15 18:26:34 +0000

    Fix: PEP8 Style

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 
 
 def no_c(my_string):
-        new_string = ''
-        for i in my_string:
-                if not i in ['c', 'C']:
-                        new_string += i
-        return new_string
+    new_string = ''
+    for i in my_string:
+        if not i in ['c', 'C']:
+            new_string += i
+    return new_string
