ec405118e846a03ac973e989e07e0041bee892f3
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-04 21:11:24 +0000

    Test files

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-common_elements = __import__('3-common_elements').common_elements
+square_matrix_map = \
+    __import__('101-square_matrix_map').square_matrix_map
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_map(matrix)
+print(new_matrix)
+print(matrix)
