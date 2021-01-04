86aecbc303ed5b69267885c50d1a9cc3a5229763
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-04 17:23:57 +0000

    Prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/urs/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    try:
+        while num in range(x):
+            print(my_list[num], end='')
+            num += 1
+    except IndexError:
+        None
+    print()
+    return(num)
