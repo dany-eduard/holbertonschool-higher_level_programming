a22ff64bca69eaa1bb178742270874e43d1137a5
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-05 22:41:24 +0000

    A class Square that defines a square by

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Creating a new class"""
+
+
 class Square:
+    """A class Square that defines a square by"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
