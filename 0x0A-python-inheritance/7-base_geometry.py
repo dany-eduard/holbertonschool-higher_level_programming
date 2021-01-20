ad5301a6f227bbd0738f5cfa6bc2225a4f84621c
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-20 03:57:12 +0000

    Add: public instance method: def area(self): that raises an Exception

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,18 +14,7 @@ class BaseGeometry:
         raise Exeption('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """Public instance method that validates value
-
-        Args:
-            name (str): you can assume name is always a string
-            value (int): validate, must be an int
-
-        Raises:
-            TypeError: if value is not an integer: raise a TypeError \
-                exception, with the message <name> must be an integer.
-            ValueError: if value is less or equal to 0: raise a ValueError \
-                exception with the message <name> must be greater than 0.
-        """
+        """Public instance method that validates value"""
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
