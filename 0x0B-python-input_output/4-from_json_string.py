0ab0ee18edfe3c71b3655c6253858e08689d5e6e
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-20 23:02:54 +0000

    Return JSON in string format

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns an object
+(Python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """Return JSON in string format"""
+    return json.loads(my_str)
