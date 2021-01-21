543b37ef741fd3a0298bf869031034fe948a60e7
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-21 01:25:43 +0000

    Script that add all arguments to a Python list and then save them to a JSON file Fix: 7-add_item.py:3:80: E501 line too long (80 > 79 characters)

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,5 @@ import json
 def load_from_json_file(filename):
     """Create an Object from a JSON file"""
     with open(filename, 'r') as f:
+        # load: lee un archivo y los datos JSON los convierte en objeto
         return json.load(f)
