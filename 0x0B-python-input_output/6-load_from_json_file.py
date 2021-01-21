5dad426beecbecdc2f796bb437ede7a834be7f92
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-21 01:43:00 +0000

    Create an Object from a JSON file Add: Comentario para load()

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """Create an Object from a JSON file"""
     with open(filename, 'r') as f:
-        # load: lee un archivo y los datos JSON los convierte en objeto
+        # load: lee un archivo y convierte los datos en JSON(objeto)
         return json.load(f)
