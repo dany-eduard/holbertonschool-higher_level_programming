768d6222221e0aa21b2c901d516574d1112e4639
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-21 01:38:28 +0000

    Write an Objet to a text file (.json) Add: comentario a dump()

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,6 +9,7 @@ import json
 def save_to_json_file(my_obj, filename):
     """Write an Objet to a text file (.json)"""
     with open(filename, 'w') as f:
+        # dump: crea un objeto JSON y lo guarda en un archivo
         json.dump(my_obj, f)
         """ json_data = json.dumps(my_obj)
         f.write(json_data) """
