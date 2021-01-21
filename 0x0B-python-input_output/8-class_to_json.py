e1223bb348405c928f6c07ac70716acdc159eb8a
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-21 01:56:00 +0000

    Return the dictionary description for a class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that  returns the dictionary
+description with simple data structure (list, dictionary, 
+string, integer and boolean) for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """Return the dictionary description"""
+    return obj.__dict__ # or vars(obj)
