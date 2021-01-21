5b12f3c83e60792633d79d97b41e895591b5504a
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-21 02:20:55 +0000

    Student class Add: If attrs is a list of strings, only attribute names contained in this list must be retrieved.

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a class Student that defines a student
+"""
+
+
+class Student():
+    """
+    Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """JSON representation"""
+        if attrs is None:
+            return self.__dict__  # or return vars(obj)
+        else:
+            attrs_dic = {}
+            for i in self.__dict__:
+                if i in attrs:
+                    attrs_dic[i] = self.__dict__[i]
+            return attrs_dic
