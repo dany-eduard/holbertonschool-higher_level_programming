5ae6a3549b090aa900e7ac2d8af73ded9ef3e6e3
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-22 21:34:42 +0000

    Returns a list of lists of Pascal’s triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains the function pascal_triangle"""
+
+
+def pascal_triangle(n):
+    mtx = []
+    aux = 0
+    for i in range(n):
+        mtx.append([])
+    for i in range(n):
+        for x in range(i + 1):
+            if x == 0 or x == i:
+                mtx[i].append(1)
+            else:
+                aux = mtx[i - 1][x] + mtx[i - 1][x - 1]
+                mtx[i].append(aux)
+        """ print(mtx[i]) """
+    return mtx
