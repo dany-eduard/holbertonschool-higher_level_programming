5b0b7ede5d9a6d43cdbc86f83dcf8f6b6066b67e
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-22 19:39:17 +0000

    Fix: PEP8 Style 8-class_to_json.py:4:58: W291 trailing whitespace

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module contains a function that  returns the dictionary
-description with simple data structure (list, dictionary, 
+description with simple data structure (list, dictionary,
 string, integer and boolean) for JSON serialization of an object
 """
 
