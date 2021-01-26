50fc23badacf51b4472b9e763af48c23495ff6b1
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-01-26 00:30:58 +0000

    Created: Class Square that inherits from Rectangle
    And Update: the class Square by adding the public getter and setter size.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains the Square Class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square Class extends from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter - obtain the size of the Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter - define the size value of this Square"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return the str representation of Square"""
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.size)
