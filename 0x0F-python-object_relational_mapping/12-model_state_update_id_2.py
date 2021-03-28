42a2f9c69158aac671c1186a98e59895f36142d3
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-28 22:46:56 +0000

    Changes the name of a State object from the database

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that changes the name of a State object from the database
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+
+    sesion = sessionmaker(bind=engine)
+    session = sesion()
+    search_state = session.query(State).filter(State.id == 2).first()
+    search_state.name = 'New Mexico'
+    session.commit()
+    session.close()
