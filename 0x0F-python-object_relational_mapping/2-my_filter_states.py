2e3aa841032cd383201db98dac31f9efd1c4f5c3
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-28 14:52:29 +0000

    Tthat takes in an argument and displays all values in the states table where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,16 +2,14 @@
 
 import MySQLdb
 from sys import argv
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
+    db = MySQLdb.connect(user=argv[1],
                          passwd=argv[2],
                          db=argv[3],
+                         host="localhost",
                          port=3306)
 
     cursor = db.cursor()
-
     upper = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"\
             .format(argv[4])
     cursor.execute(upper)
