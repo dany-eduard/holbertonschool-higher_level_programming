10411387a72d47b66e96474a47a4b0f26d2c484b
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-09 03:21:31 +0000

    Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,7 @@
+#!#!/usr/bin/env bash
+# Script that takes in a URL as an argument, sends a GET
+# request to the URL, and displays the body of the response.
+# A header variable X-HolbertonSchool-User-Id must be sent
+# with the value 98.
+
+curl -sH "X-HolbertonSchool-User-Id: 98" $1
