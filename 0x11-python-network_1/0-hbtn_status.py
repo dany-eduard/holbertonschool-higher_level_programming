86db7e5863b09d261c927560d71489544d44d259
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-09 23:31:55 -0500

    Update task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,7 @@
 """ script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+if __name__ == "__main__":
     """
     Body response:
         - type: <class 'bytes'>
