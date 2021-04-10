e50fcb19cde378ea8d3905841840dede945739a5
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-09 21:47:39 -0500

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a POST request with some parameters
+# Sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -s -X HEAD -w "%{http_code}" "$1"
