8ffb87653e1f814dfb4e44275c600418e38d9b42
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-09 19:13:36 -0500

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a POST request to the passed URL,
-# and displays the body of the response.
+# Takes in a URL, sends a POST request to the passed URL, displays the body of the response.
 curl -s -X POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
