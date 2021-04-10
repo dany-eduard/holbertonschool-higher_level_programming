10923312376e4eea719d3df66adbcc2cbdf8cf88
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-04-09 21:51:04 -0500

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL passed as the first argument,
 # and displays the body of the response.
-curl -sd "@$2" -H "Content-Type: application/json" -X POST $1
+curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2"
