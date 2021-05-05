e535efc864a779ba294891ba9f2d6a70fea9214c
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-05-04 22:17:46 -0500

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+let count = 0;
 
-request.get(url, (err, body) => {
+request.get(url, (err, res, body) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    let count = 0;
-    for (let film of JSON.parse(body).results) {
-      film.characters.forEach((character) => {
-        if (character.includes('18')) {
-          count++;
+    const movies = JSON.parse(body).results;
+    for (let i = 0; i < movies.length; i++) {
+      const characters = movies[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count += 1;
         }
-      })
+      }
     }
     console.log(count);
   }
