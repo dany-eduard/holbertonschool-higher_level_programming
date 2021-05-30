5e95966d20fbf6e2f20b62f4ce2bc554699cf00c
dany-eduard <danyeduard17@gmail.com>
2021-05-29 21:29:34 -0500

    Add: advanced tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Catch me if you can!
-curl -sL -X PUT -d user_id=98 -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL http://0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
 
